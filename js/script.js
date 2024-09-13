0e67af60c265c99f5b6889566d403de767d3ef24
RoGo117 <rodrigoclases117@gmail.com>
2024-09-13 17:07:54 +0200

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* Prueba GitHub */
+
+/* Abrir terminal: Ctrl + ñ
+   (Se necesita tener una cuenta de GitHub y tener git descargado en el equipo)
+   
+ - git init
+
+ - git add index.html                      (para añadir el fichero concreto index.html)
+ - git add *.css                           (para añadir los ficheros .css)
+ - git add .                               (para añadir todo)
+
+ - git status -s                           (para saber en qué estado están los ficheros)
+
+ - git commit -m "Initial commit"          (para documentar los cambios con el nombre "Initial commit")
+ - git log                                 (para ver el registro de los commits, muestra la versión también)
+ - git log --oneline                       (igual al git log, pero escrito todo en la misma línea)
+
+ - git reset --hard dec1d9996fc...         (para cargar a una versión anterior)
+ - git reflog                              (como un log, pero también muestra los commits borrados o cambiados)
+
+*/
+
+/* Para subirlo como repositorio, crear uno nuevo desde GitHub, configurarlo (ponerlo privado y darle nombre)
+   y copiar el segundo cuadro de comandos que aparece una vez creado.   
+
+      (Ejemplo de caso:)
+
+      git remote add origin https://github.com/RoGo117/Pruebas-DIW.git
+      git branch -M main
+      git push -u origin main
+
+  Copiar y ejecutar los comandos desde el terminal y refrescar la página del repositorio para que aparezca el contenido
+
+*/
